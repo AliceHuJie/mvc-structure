682535c456f3bfa0e17ec21cd77c222e7c4f483b
1262741800@qq.com <hujie20122736>
2020-01-16 00:00:31 +0800

    dispatchServlet初始化： 1.加载配置文件 2.扫描指定路径下的类

--- src/main/java/com/example/mvcframework/servlet/MyDispatcherServlet.java
+++ src/main/java/com/example/mvcframework/servlet/MyDispatcherServlet.java
@@ -5,9 +5,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class MyDispatcherServlet extends HttpServlet {
+    private Properties contextConfig = new Properties();
+    private List<String> classNames = new ArrayList<>();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
@@ -15,14 +24,68 @@ public class MyDispatcherServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        // 6. 等待请求阶段
+        req.getRequestURI();
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        // 加载配置文件
-        System.out.println("进入自己的dispatch servlet");
-        super.init(config);
+        // 1. 加载配置文件
+        doLoadConfig(config.getInitParameter("contextConfigLocation"));
+        // 2. 扫描所有相关的类
+        doScanner(contextConfig.getProperty("scanPackage"));
+        // 3. 初始化所有相关的类并且将其保存到IOC容器中
+        doInstance();
+        // 4. 执行依赖注入 把加了autowired注解的字段赋值
+        doAutowired();
+        // 5. 构造handleMapping, 将url和方法进行关联
+        initHandleMapping();
+
+        System.out.println("HJ MVC FRAMEWORK IS INIT.");
     }
 
+    // 1. 加载配置文件
+    private void doLoadConfig(String contextConfigLocation) {
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(contextConfigLocation);
+        try {
+            contextConfig.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != resourceAsStream) {
+                try {
+                    resourceAsStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 2. 扫描所有相关的类
+    private void doScanner(String basePackage) {
+        URL url = this.getClass().getClassLoader().getResource("/" + basePackage.replaceAll("\\.", "/"));
+        File dir = new File(url.getFile());
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                doScanner(basePackage + "." + file.getName());
+            } else {
+                String className = basePackage + "." + file.getName().replace(".class", "");
+                classNames.add(className);
+                System.out.println(className);
+            }
+        }
+    }
+
+    // 3. 初始化所有相关的类并且将其保存到IOC容器中
+    private void doInstance() {
+    }
+
+    // 4. 执行依赖注入 把加了autowired注解的字段赋值
+    private void doAutowired() {
+    }
+
+    // 5. 构造handleMapping, 将url和方法进行关联
+    private void initHandleMapping() {
+    }
 }
